49f8401e51592fdb75f15e272cbdc1ec392f2710
SamPrim <ndostephan04@gmail.com>
2022-03-17 03:42:17 -0700

    number have life

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -16,8 +16,6 @@ int main(void)
   print_number(0);
   _putchar('\n');
   print_number(-98);
-  _putchar('\n');
-  print_number(-987654321);
   _putchar('\n');
   return (0);
 }
